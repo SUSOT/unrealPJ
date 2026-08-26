0d5e2dfd4cabeb4c9c0e13b84508b1e6bfc913d1
moon080411 <157565761+moon080411@users.noreply.github.com>
2026-08-26 11:57:40 +0900

    feat : GameMode

---
+++ Source/UnrealTeamPJ/System/UTPGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UTPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTEAMPJ_API AUTPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
